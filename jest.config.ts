e926f0a2f8357160c392936f783738d345c4b7b3
Jairo Gomes <evaristojairo12@gmail.com>
2021-04-27 17:39:37 -0300

    :wrench: configuration tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -147,7 +147,6 @@ export default {
 
   // The glob patterns Jest uses to detect test files
   testMatch: ["**/__tests__/*.test.ts"],
-
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
   //   "\\\\node_modules\\\\"
