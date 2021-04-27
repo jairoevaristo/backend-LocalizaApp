a1688bccaf9a54728b931755ae2df2bb5da21494
Jairo Gomes <evaristojairo12@gmail.com>
2021-04-27 20:38:08 -0300

    :card_file_box: create migration the establishment

---
+++ src/database/migrations/1619565783622-createEstablishment.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createEstablishment1619565783622 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'establishment',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        {
+          name: 'description',
+          type: 'varchar'
+        },
+        {
+          name: 'contact',
+          type: 'varchar'
+        },
+        {
+          name: 'category',
+          type: 'varchar'
+        },
+        {
+          name: 'latitude',
+          type: 'double precision'
+        },
+        {
+          name: 'longitude',
+          type: 'double precision'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        }
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('establishment');
+  }
+
+}
